434a297a817b11b18341ba93e804704efcc8ddda
Sanjay Ghemawat <sanjay@alum.mit.edu>
2014-07-03 09:24:06 -0700

    moved Find to its own file

--- pipe.go
+++ pipe.go
@@ -10,7 +10,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"regexp"
 )
 
@@ -93,33 +92,6 @@ func Numbers(x, y int) Filter {
 		for i := x; i <= y; i++ {
 			arg.Out <- fmt.Sprintf("%d", i)
 		}
-	}
-}
-
-// FindMatch is a mask that controls the types of nodes emitted by Find.
-type FindMatch int
-
-const (
-	FILES    FindMatch = 1    // Match regular files
-	DIRS               = 2    // Match directories
-	SYMLINKS           = 4    // Match symbolic links
-	ALL                = 0xff // Match everything
-)
-
-// Find copies all input and then produces a sequence of items, one
-// per file/directory/symlink found at or under dir that matches mask.
-func Find(mask FindMatch, dir string) Filter {
-	return func(arg Arg) {
-		passThrough(arg)
-		filepath.Walk(dir, func(f string, s os.FileInfo, e error) error {
-			if mask&ALL == ALL ||
-				mask&FILES != 0 && s.Mode().IsRegular() ||
-				mask&DIRS != 0 && s.Mode().IsDir() ||
-				mask&SYMLINKS != 0 && s.Mode()&os.ModeSymlink != 0 {
-				arg.Out <- f
-			}
-			return nil
-		})
 	}
 }
 

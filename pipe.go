3609e8d00a46812fed29fd7df9eb4762c3fdd03b
Sanjay Ghemawat <sanjay@alum.mit.edu>
2014-07-03 09:22:31 -0700

    moved ParallelMap to its own file

--- pipe.go
+++ pipe.go
@@ -12,7 +12,6 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
-	"sync"
 )
 
 // Arg contains the data passed to a Filter. The important parts are
@@ -218,61 +217,6 @@ func UniqWithCount() Filter {
 		if count > 0 {
 			arg.Out <- fmt.Sprintf("%d %s", count, current)
 		}
-	}
-}
-
-type parItem struct {
-	index int
-	value string
-}
-
-// ParallelMap calls fn(x) for every item x in a pool of n
-// goroutines and yields the outputs of the fn calls. The output order
-// matches the input order.
-func ParallelMap(n int, fn func(string) string) Filter {
-	return func(arg Arg) {
-		// Attach a sequence number to each item.
-		source := make(chan parItem, 10000)
-		go func() {
-			i := 0
-			for s := range arg.In {
-				source <- parItem{i, s}
-				i++
-			}
-			close(source)
-		}()
-
-		// We keep track of outputs in a map indexed by the
-		// sequence number of the item.  These items are
-		// yielded in order.
-		var mu sync.Mutex
-		buffered := make(map[int]string)
-		next := 0
-		nextItemReady := func() bool {
-			_, ok := buffered[next]
-			return ok
-		}
-
-		// Process the items in n go routines.
-		var wg sync.WaitGroup
-		wg.Add(n)
-		for i := 0; i < n; i++ {
-			go func() {
-				for item := range source {
-					s := fn(item.value)
-					mu.Lock()
-					buffered[item.index] = s
-					for nextItemReady() {
-						arg.Out <- buffered[next]
-						delete(buffered, next)
-						next++
-					}
-					mu.Unlock()
-				}
-				wg.Done()
-			}()
-		}
-		wg.Wait()
 	}
 }
 

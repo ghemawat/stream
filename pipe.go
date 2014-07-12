265b2c99f5ef70e67f2ed22d134dc7864e1cbf0b
Sanjay Ghemawat <sanjay@alum.mit.edu>
2014-07-12 11:01:52 -0700

    moved regexp filters to their own file

--- pipe.go
+++ pipe.go
@@ -46,7 +46,6 @@ package pipe
 
 import (
 	"fmt"
-	"regexp"
 	"sync"
 )
 
@@ -178,24 +177,6 @@ func If(fn func(string) bool) Filter {
 	}
 }
 
-// Grep emits every input x that matches the regular expression r.
-func Grep(r string) Filter {
-	re, err := regexp.Compile(r)
-	if err != nil {
-		return func(Arg) error { return err }
-	}
-	return If(re.MatchString)
-}
-
-// GrepNot emits every input x that does not match the regular expression r.
-func GrepNot(r string) Filter {
-	re, err := regexp.Compile(r)
-	if err != nil {
-		return func(Arg) error { return err }
-	}
-	return If(func(s string) bool { return !re.MatchString(s) })
-}
-
 // Uniq squashes adjacent identical items in arg.In into a single output.
 func Uniq() Filter {
 	return func(arg Arg) error {
@@ -230,22 +211,6 @@ func UniqWithCount() Filter {
 		}
 		if count > 0 {
 			arg.Out <- fmt.Sprintf("%d %s", count, current)
-		}
-		return nil
-	}
-}
-
-// Substitute replaces all occurrences of the regular expression r in
-// an input item with replacement.  The replacement string can contain
-// $1, $2, etc. which represent submatches of r.
-func Substitute(r, replacement string) Filter {
-	return func(arg Arg) error {
-		re, err := regexp.Compile(r)
-		if err != nil {
-			return err
-		}
-		for s := range arg.In {
-			arg.Out <- re.ReplaceAllString(s, replacement)
 		}
 		return nil
 	}

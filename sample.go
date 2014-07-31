aaef0d69e65e00440e1fe1a280d8e9b346f1b6ad
Sanjay Ghemawat <sanjay@alum.mit.edu>
2014-07-31 06:54:27 -0700

    Added comments based on feedback from Nigel

--- sample.go
+++ sample.go
@@ -6,6 +6,7 @@ import (
 )
 
 // Sample picks n pseudo-randomly chosen input items.
+// TODO: Maybe add SampleWithSeed.
 func Sample(n int) Filter {
 	return FilterFunc(func(arg Arg) error {
 		// Could speed this up by using Algorithm Z from Vitter.
@@ -17,6 +18,9 @@ func Sample(n int) Filter {
 			case i < n:
 				reservoir = append(reservoir, s)
 			case r.Float32() < float32(n)/float32(i+1):
+				// TODO: Maybe use float64.  Or try
+				//   j := r.Intn(i+1)
+				//   if j < n { reservoir[j] = s }
 				reservoir[r.Intn(n)] = s
 			}
 			i++

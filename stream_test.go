8328c123f2d13b7e7bd39d68366c92f8f598759a
Sanjay Ghemawat <sanjay@alum.mit.edu>
2014-08-16 07:55:23 -0700

    moved benchmarks to their own file

--- stream_test.go
+++ stream_test.go
@@ -6,7 +6,6 @@ import (
 	"bytes"
 	"fmt"
 	"os"
-	"testing"
 )
 
 func ExampleSequence() {
@@ -526,44 +525,4 @@ func ExampleXargs_splitArguments() {
 	)
 	// Output:
 	// 3
-}
-
-func BenchmarkSingle(b *testing.B) {
-	stream.Run(stream.Repeat("", b.N))
-}
-
-func BenchmarkFive(b *testing.B) {
-	f := stream.FilterFunc(func(arg stream.Arg) error {
-		for s := range arg.In {
-			arg.Out <- s
-		}
-		return nil
-	})
-	stream.Run(stream.Repeat("", b.N), f, f, f, f)
-}
-
-func BenchmarkWL(b *testing.B) {
-	f, err := os.Create("/dev/null")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	stream.Run(
-		stream.Repeat("hello", b.N),
-		stream.WriteLines(f),
-	)
-}
-
-func BenchmarkSample(b *testing.B) {
-	stream.Run(
-		stream.Repeat("hello", b.N),
-		stream.Sample(10),
-	)
-}
-
-func BenchmarkSort(b *testing.B) {
-	stream.Run(
-		stream.Repeat("hello", b.N),
-		stream.Sort(),
-	)
 }
